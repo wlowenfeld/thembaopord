02d0fc55599a5c5dab1c1f588c214c5367c45cd6
wlowenfeld <wlowenfeld@gmail.com>
2026-03-06 08:30:10 -0500

    Delete lib directory

--- lib/markdown.ts
+++
@@ -1,9 +0,0 @@
-import { micromark } from "micromark";
-
-/**
- * Convert markdown string to HTML using micromark.
- * Used for rendering blog post content from .mdx files.
- */
-export function markdownToHtml(content: string): string {
-  return micromark(content);
-}
